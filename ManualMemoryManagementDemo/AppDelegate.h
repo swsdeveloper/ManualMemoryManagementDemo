d5b3e325dedf8785fb5f5661a8fbcb314636ff52
Steven Shatz <sshatzdeveloper@gmail.com>
2014-10-26 17:23:49 -0400

    Initial Commit

---
+++ ManualMemoryManagementDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ManualMemoryManagementDemo
+//
+//  Created by Steven Shatz on 10/26/14.
+//  Copyright (c) 2014 Steven Shatz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ManualMemoryManagementDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ManualMemoryManagementDemo
+//
+//  Created by Steven Shatz on 10/26/14.
+//  Copyright (c) 2014 Steven Shatz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
